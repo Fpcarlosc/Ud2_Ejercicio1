f56e4c882cb7ca8ca207d53ef579edf2ea7ff0b3
User <user@es.es>
2024-09-29 07:10:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ud2_ejercicio1/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.ud2_ejercicio1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ud2_ejercicio1.ui.theme.Ud2_Ejercicio1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ud2_Ejercicio1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Ud2Ejercicio1App(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Ud2Ejercicio1App(modifier: Modifier = Modifier) {
+    ImagenRandomBoton(modifier= modifier)
+}
+
+@Composable
+fun ImagenRandomBoton(modifier: Modifier = Modifier) {
+    // Para poder cambiar el valor de la variable aleatoria debemos guardar su estado.
+    var result by remember { mutableStateOf(1) }
+
+    // Dependiendo del valor de la variable aleatoria cargaremos una imagen u otra.
+    val imagen = when(result) {
+        1 -> R.drawable.bandera1
+        2 -> R.drawable.bandera2
+        3 -> R.drawable.bandera3
+        4 -> R.drawable.bandera4
+        else -> R.drawable.bandera5
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imagen),
+            contentDescription = stringResource(R.string.bandera, result)
+        )
+        Button(
+            onClick = {result = (1..6).random() }
+        ) {
+            Text(text = stringResource(R.string.tira))
+        }
+    }
+}
+
